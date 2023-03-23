60e8aa3fcab13392ff863f29549c2449bf602f8a
mshaari <michael.shaari@gmail.com>
2023-03-22 22:42:17 -0400

    Fully functional application

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -26,7 +26,7 @@ module.exports = {
     Thought.create(req.body)
       .then((thought) => {
         return User.findOneAndUpdate(
-          { _id: req.body.userId },
+          { username: req.body.username },
           { $addToSet: { thoughts: thought._id } },
           { new: true }
         );
